5f203b0f311ecc4f9b6dc8b5627a5ed6098f1419
Administrator <755907724@qq.com>
2017-11-06 20:42:18 +0800

    第一次提交

---
+++ application/admin/common.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/6 0006
+ * Time: 下午 1:54
+ */
---
+++ application/admin/controller/Index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/6 0006
+ * Time: 下午 1:56
+ */
+
+
+namespace app\admin\controller;
+use think\Controller;
+class Index extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+    public function test()
+    {
+        return view();
+    }
+    public function tt()
+    {
+        $this->view->engine->layout(true);
+        return view();
+    }
+}
