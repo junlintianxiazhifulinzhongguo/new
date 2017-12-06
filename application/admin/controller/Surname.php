119d5008505baa8319d576ce3e672ec15f201c19
Administrator <755907724@qq.com>
2017-12-06 21:38:03 +0800

    最后一步融合

--- application/admin/controller/Surname.php
+++ application/admin/controller/Surname.php
@@ -8,7 +8,7 @@
 
 namespace app\admin\controller;
 use think\Controller;
-class Index extends Controller
+class Surname extends Controller
 {
     public function index()
     {
--- application/admin/model/Surname.php
+++ application/admin/model/Surname.php
@@ -5,16 +5,22 @@ class Surname extends Model
 {
     /**
      * 获取姓氏
+     * @param int $search
+     * @param int $order
      * @param int $location,起始位置
      * @param int $length
      * @return bool
      */
-    public function getSurname($order,$location=0,$length=5)
+    public function getSurname($search,$order,$location=0,$length=5)
     {
 
-        return $this->order($order)->limit($location,$length)->select();
+        return $this->where('name|pinyin','like','%'.$search.'%')->order($order)->limit($location,$length)->select();
     }
 
+    public function seach($keywords)
+    {
+        return $this->where('name|pinyin','like','%'.$keywords.'%')->select();
+    }
 
 
 }
